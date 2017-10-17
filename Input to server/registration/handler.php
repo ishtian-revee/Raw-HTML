62f76f5504965e70b96136db8b0e0d34f128d84e
Ishtian Revee <ishtian11@gmail.com>
2017-10-17 21:10:24 +0600

    sources added

---
+++ Input to server/registration/handler.php
@@ -0,0 +1,12 @@
+<?php
+
+  $name = $_REQUEST['name'];
+  $email = $_REQUEST['email'];
+  $username = $_REQUEST['username'];
+  $password = $_REQUEST['password'];
+  $confirm = $_REQUEST['confirm'];
+  $gender = $_REQUEST['gender'];
+  $day = $_REQUEST['day'];
+  $month = $_REQUEST['month'];
+  $year = $_REQUEST['year'];
+?>
